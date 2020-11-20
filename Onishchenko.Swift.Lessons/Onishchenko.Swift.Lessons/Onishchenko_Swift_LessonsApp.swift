21b1f13a6c4de6251402d62a4e63fd1789dcc273
Anton <good-price0@mail.ru>
2020-11-20 17:06:57 +0300

    Added test XCode Project

---
+++ Onishchenko.Swift.Lessons/Onishchenko.Swift.Lessons/Onishchenko_Swift_LessonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Onishchenko_Swift_LessonsApp.swift
+//  Onishchenko.Swift.Lessons
+//
+//  Created by Onishchenko Anton on 20.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct Onishchenko_Swift_LessonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Onishchenko.Swift.Lessons/Onishchenko.Swift.LessonsTests/Onishchenko_Swift_LessonsTests.swift
@@ -0,0 +1,33 @@
+//
+//  Onishchenko_Swift_LessonsTests.swift
+//  Onishchenko.Swift.LessonsTests
+//
+//  Created by Onishchenko Anton on 20.11.2020.
+//
+
+import XCTest
+@testable import Onishchenko_Swift_Lessons
+
+class Onishchenko_Swift_LessonsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
